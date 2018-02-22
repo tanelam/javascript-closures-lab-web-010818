edf09bcf7786b7f5821894e7c35bbbef5a2e5ba5
tanelam <tania.e.aparicio@gmail.com>
2018-02-21 19:07:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  let counter = 0;
+
+  function addBump(){
+   return counter++;
+  };
+
+  function getBumps(){
+    return counter;
+  };
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType){
+  return function(deadlyDevice){
+  return {animalType, deadlyDevice}
+  };
+}
+
+let sharkCreator = createAnimal("Shark");
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+
+let sharkWithFrickinCannon = sharkCreator('Cannon');
